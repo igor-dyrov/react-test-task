b971cc38baf28ccab56e79adeaf358498ae2495d
igor-dyrov <bazbazbaz.id@gmail.com>
2019-04-03 19:25:14 +0300

    done mvp

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -7,33 +7,115 @@ import { sortDataByYears, initDataObject, sortDataByMonths, sortDataByDays } fro
 import './Main.css';
 
 class Main extends React.Component {
-	componentDidMount() {
-		setTimeout(() => this.props.setSortingByDays(2013, 'February'), 1000);
+	constructor(props) {
+		super(props);
+		this._yearHandler = this.handleYearSelecting.bind(this);
+		this._monthHandler = this.handleMonthSelecting.bind(this);
+		this._selectHandler = this.handleSelectVisibility.bind(this);
+		this.state = {
+			yearSelectIsVisible: false,
+			monthSelectIsVisible: false,
+			selectedYear: '',
+			selectedMonth: '',
+		};
+	}
+
+	componentWillMount() {
 		const { initGraphData } = this.props;
+		const { setSortingByYears } = this.props;
 		fetch('http://localhost:8000/api/data')
 			.then((response) => response.json())
-			.then((response) => initGraphData(response));
+			.then((response) => {
+				initGraphData(response);
+				const { years } = this.props;
+				const { months } = this.props;
+				this.setState({
+					selectedYear: years[0],
+					selectedMonth: months[0],
+				});
+				setSortingByYears();
+			});
 	}
-	
+
+	handleSelectVisibility(event) {
+		const { setSortingByYears } = this.props;
+		const { setSortingByMonths } = this.props;
+		const { setSortingByDays } = this.props;
+		const { selectedYear } = this.state;
+		const { selectedMonth } = this.state;
+		if (event.target.value === 'Month') {
+			this.setState({
+				yearSelectIsVisible: true,
+				monthSelectIsVisible: false,
+			});
+			setSortingByMonths(selectedYear);
+		} else if (event.target.value === 'Day') {
+			this.setState({
+				yearSelectIsVisible: true,
+				monthSelectIsVisible: true,
+			});
+			setSortingByDays(selectedYear, selectedMonth);
+		} else {
+			this.setState({
+				yearSelectIsVisible: false,
+				monthSelectIsVisible: false,
+			});
+			setSortingByYears();
+		}
+	}
+
+	handleYearSelecting(event) {
+		const { setSortingByMonths } = this.props;
+		const { setSortingByDays } = this.props;
+		const { monthSelectIsVisible } = this.state;
+		const { selectedMonth } = this.state;
+		this.setState({
+			selectedYear: event.target.value
+		});
+		if (monthSelectIsVisible) {
+			setSortingByDays(event.target.value, selectedMonth);
+		} else {
+			setSortingByMonths(event.target.value);
+		}
+	}
+
+	handleMonthSelecting(event) {
+		const { setSortingByDays } = this.props;
+		const { selectedYear } = this.state;
+		this.setState({
+			selectedMonth: event.target.value
+		});
+		setSortingByDays(selectedYear, event.target.value);
+	}
+
 	render() {
 		const { barData } = this.props;
 		const { years } = this.props;
 		const { months } = this.props;
-		
+		const { yearSelectIsVisible } = this.state;
+		const { monthSelectIsVisible } = this.state;
+
 		return (
 			<main>
 				<div className='main-block__control'>
-					<select className='main-block__control-select'>
-						{years.map((year) => <option key={year}>{year}</option>)}
+					<select onClick={this._selectHandler}>
+						<option value='Year'>Year</option>
+						<option value='Month'>Month</option>
+						<option value='Day'>Day</option>
 					</select>
-					<select>
-						{months.map((month) => <option key={month}>{month}</option>)}
-					</select>
+					{yearSelectIsVisible ? (
+						<select onClick={this._yearHandler}>
+							{years.map((year) => <option key={year} value={year}>{year}</option>)}
+						</select>
+					) : null}
+					{monthSelectIsVisible ? (
+						<select onClick={this._monthHandler}>
+							{months.map((month) => <option key={month}>{month}</option>)}
+						</select>
+					) : null}
 				</div>
 				<div className='main-block__bar'>
-					<Bar
-						data={barData}
-					/>
+					<Bar data={barData}/>
 				</div>
 			</main>
 		);
@@ -41,7 +123,7 @@ class Main extends React.Component {
 }
 
 Main.propTypes = {
-	barData: PropTypes.array,
+	barData: PropTypes.object,
 	months: PropTypes.array,
 	years: PropTypes.array,
 	setSortingByYears: PropTypes.func,
--- src/redux/graph/graph.reducer.js
+++ src/redux/graph/graph.reducer.js
@@ -10,7 +10,7 @@ function getRandomColor() {
 
 const initialState = {
 	initialObject: [],
-	graphData: [],
+	graphData: {},
 	years: [],
 	months: [],
 };
